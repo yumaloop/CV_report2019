ebef16bb08296f7429f145e58a34e315d30d30f4
yumaloop <yumaloop@gmail.com>
2019-07-21 18:51:16 +0900

    update repo 2019年 7月21日 日曜日 18時51分16秒 JST

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,18 @@
-git add .
-git commit -m "update"
-git push origin master
+#!/bin/bash
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+# Go To .git root directory
+cd ~/workspace/CV_report2019
+
+# Add all changes to git.
+git add .
+
+# Commit changes.
+msg="update repo `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
